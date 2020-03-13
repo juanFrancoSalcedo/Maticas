891b9daa130ccda9e248896bd731aa34eea1b09f
juanFrancoSalcedo <62152724+juanFrancoSalcedo@users.noreply.github.com>
2020-03-13 18:00:48 -0500

    Maticas Prubva

---
+++ PitacosMaths/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+
+    private SpriteRenderer renderSprite;
+    public Sprite xSprite;
+    private Sprite ySprite;
+    [SerializeField] private GridManger grid;
+
+    
+    void Start()
+    {
+        renderSprite = GetComponent<SpriteRenderer>();
+        ySprite = renderSprite.sprite;
+        transform.position = new Vector3(grid.playerX,grid.playerY,transform.position.z);
+    }
+
+    private void Update()
+    {
+
+    }
+
+    public IEnumerator Move()
+    {
+        while (transform.position != new Vector3(grid.targetX,grid.targetY,0))
+        {
+            if (grid.targetX != (int)transform.position.x)
+            {
+                int dif = (grid.targetX > (int)transform.position.x) ? 1 : -1;
+
+                renderSprite.sprite = xSprite;
+                renderSprite.flipX = (dif > 0) ? false : true;
+                transform.Translate(dif, 0, 0);
+
+            }
+            else if (grid.targetY != (int)transform.position.y)
+            {
+                    int dif = (grid.targetY > (int)transform.position.y) ? 1 : -1;
+
+                    renderSprite.sprite = ySprite;
+                    renderSprite.flipY = (dif > 0) ? true : false;
+                    transform.Translate(0, dif, 0);
+
+
+            }
+
+            yield return new WaitForSeconds(0.3f);
+        }
+    }
+
+
+}
---
+++ PitacosMaths/Assets/Scripts/InputFieldController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class InputFieldController : MonoBehaviour
+{
+    [SerializeField] private TMP_InputField xInputField;
+    [SerializeField] private TMP_InputField yInputField;
+    [SerializeField] private Button startPathButton;
+
+    [SerializeField] private GridManger grid;
+    [SerializeField] private CharacterController character;
+    
+    void Start()
+    {
+        startPathButton.onClick.AddListener(StartPath);
+    }
+
+
+    public void StartPath()
+    {
+        int xCordinate =  (xInputField.text.Equals("")) ? 0 : int.Parse(xInputField.text);
+        int yCordinate =  (yInputField.text.Equals("")) ? 0 : int.Parse(yInputField.text);
+
+        grid.targetX = xCordinate;
+        grid.targetY = yCordinate;
+
+        character.StartCoroutine(character.Move());
+
+        startPathButton.gameObject.SetActive(false);
+        xInputField.gameObject.SetActive(false);
+        yInputField.gameObject.SetActive(false);
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
